ff9f210d83bd500a4deb7bfb2117cc826eccadd8
Noah Hüsser <nh@technokrat.ch>
2020-10-16 11:45:55 +0200

    More minimal

--- src/main.rs
+++ src/main.rs
@@ -15,37 +15,12 @@ use types::R;
 const APP: () = {
     struct Resources {
         sin_buffer: [R; SAMPLE_BUFFER_SIZE],
-        cos_buffer: [f32; SAMPLE_BUFFER_SIZE],
-        samples_recorded: usize,
     }
 
     #[init(spawn = [measure])]
     fn init(ctx: init::Context) -> init::LateResources {
-        let channels = rtt_target::rtt_init! {
-            up: {
-                0: {
-                    size: 1024
-                    name: "Log"
-                }
-                1: {
-                    size: 1024
-                    name: "Debug"
-                }
-                2: {
-                    size: 1
-                    name: "Data"
-                }
-            }
-        };
-        let mut info = channels.up.0;
-        let data = channels.up.2;
-        rtt_target::set_print_channel(channels.up.1);
-        ctx.spawn.measure().unwrap();
-
         init::LateResources {
             sin_buffer: [0.0; SAMPLE_BUFFER_SIZE],
-            cos_buffer: [0.0; SAMPLE_BUFFER_SIZE],
-            samples_recorded: 0,
         }
     }
 
@@ -56,33 +31,22 @@ const APP: () = {
         }
     }
 
-    #[task(resources = [sin_buffer, cos_buffer, samples_recorded] )]
+    #[task(resources = [sin_buffer] )]
     fn measure(ctx: measure::Context) {
         static mut DAC_PHASE: u32 = 0;
 
         let measure::Context {
-            resources:
-                measure::Resources {
-                    mut sin_buffer,
-                    mut cos_buffer,
-                    mut samples_recorded,
-                },
+            resources: measure::Resources { mut sin_buffer },
             ..
         } = ctx;
 
         let sinf = 1.0f32;
-        let cosf = 1.0f32;
-        samples_recorded.lock(|samples_recorded| {
-            sin_buffer.lock(|sin_buffer| sin_buffer[*samples_recorded] = sinf);
-            cos_buffer.lock(|cos_buffer| cos_buffer[*samples_recorded] = cosf);
-        });
+        sin_buffer.lock(|sin_buffer| sin_buffer[0] = sinf);
     }
 
     #[task(
         resources = [
-            samples_recorded,
             sin_buffer,
-            cos_buffer,
         ],
         binds = EXTI0, priority = 2
     )]
